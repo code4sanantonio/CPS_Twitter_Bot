67f678cd935811f4ffcf2d81d7c27fb08b056468
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2019-05-17 17:15:05 -0500

    - Added function that removes images after they are posted to twitter

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from PIL import Image
 import datetime
 import time
 import tweepy
+import os
 
 # Twitter API Auth
 CONSUMER_KEY = ''
@@ -27,11 +28,14 @@ driver = webdriver.Chrome(DRIVER)
 
 # opens driver and takes screenshot saving it with the a datetime stamp prepended
 def open_driver():
+    # opening driver
     driver.get('http://outagemap.cpsenergy.com/CPSStaticMapsEXT/CPSStaticMapV2_EXT.html')
+    # wating for page to load
     time.sleep(5)
+    # taking screen shot of page
     driver.save_screenshot(dt + '.png')
+    # closing driver
     driver.quit()
-
 
 # Crops image then posts it to twitter
 def post_cropped_img():
@@ -39,10 +43,22 @@ def post_cropped_img():
     # uses PIL to  crop image
     image = Image.open(filename)
     cropped_image = image.crop((0, 50, 690, 685))
+    print('Cropped image')
     cropped_image.save(cropped_image_path + filename)
-
+    print('Saved cropped image')
+    
+    # uses twitter api to post image
+    api.update_with_media(cropped_image_path + filename)
+    print('posted image to twitter')
     # uses twitter api to post image
     api.update_with_media(cropped_image_path + filename)
 
+def clear_imgs():
+    for file in os.listdir('/Users/josephmartinez/PycharmProjects/CPS'):
+        if file.endswith('.png'):
+            os.remove(file)
+    print('removed images from dir')
+
 open_driver()
 post_cropped_img()
+clear_imgs()
